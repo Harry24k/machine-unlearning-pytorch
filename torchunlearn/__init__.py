8f704b8e3a9e99a5f640149ef2d816b6f35d3bdf
Chaewon Yoon <112887768+ychxnn@users.noreply.github.com>
2026-05-12 15:15:16 +0900

    Delete torchunlearn directory

--- torchunlearn/__init__.py
+++
@@ -1,9 +0,0 @@
-from .nn.robmodel import RobModel
-from .unlearn import *
-from .unlearn.rm import RecordManager
-
-from .utils import load_model
-
-from .utils.datasets import Datasets
-
-__version__ = "0.1"
--- torchunlearn/nn/modules/identity.py
+++
@@ -1,8 +0,0 @@
-# Modified from https://github.com/bearpaw/pytorch-classification/blob/master/models/cifar/wrn.py
-import torch
-import torch.nn as nn
-
-
-class Identity(nn.Module):
-    def forward(self, inputs):
-        return inputs
